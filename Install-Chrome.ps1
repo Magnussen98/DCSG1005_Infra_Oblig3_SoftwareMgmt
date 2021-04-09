66b8e98327619ab84e0c76de3cb369ea6b8b0ad0
unknown <Administrator@SEC.CORE>
2021-04-09 14:14:16 +0200

    Install

---
+++ Install-Chrome.ps1
@@ -0,0 +1,18 @@
+#### Install Google Chrome with choco ######
+
+choco install -y googleChrome
+
+##### Verify installations#########
+
+$registrylocations = (
+  "HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall",
+  "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall",
+  "HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall"
+)
+
+$registryKey = $registrylocations | Get-ChildItem | Where-Object { $_.GetValue('DisplayName') -eq 'Google Chrome'}
+
+$displayName = ($registryKey | Get-ItemProperty).DisplayName
+$version = ($registryKey | Get-ItemProperty).DisplayVersion
+
+Write-Output "Software: $displayName    -->     Version: $version"
