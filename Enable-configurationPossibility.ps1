6b927d200b1e0871b59420af9b3ff397aafcfe51
Andreas Magnussen <andrefm@stud.ntnu.no>
2021-04-09 15:36:59 +0200

    Config

---
+++ Enable-configurationPossibility.ps1
@@ -0,0 +1,29 @@
+$url = 'https://dl.google.com/dl/edgedl/chrome/policy/policy_templates.zip'
+$destination = "$HOME\Downloads\GoogleChromeEnterpriseBundle64.zip"
+$centralStoreFolder = 'C:\Windows\SYSVOL\domain\Policies\PolicyDefinitions'
+$languageFolder = $centralStoreFolder + '\en-US'
+
+#Check if 7zip is installed
+$zip = Get-Command -Name 7z.exe -ErrorAction SilentlyContinue
+
+#Download 7zip if not installed
+if ( -Not $zip ){
+    choco install -y 7zip.install
+}
+
+#Moving to Download folder
+Set-Location "$home\Downloads"
+
+# Downloading zip file from google
+Invoke-WebRequest -Uri $url -OutFile $destination
+7z x .\GoogleChromeEnterpriseBundle64.zip -ofolder
+
+# Checking if there is a central store folder for .admx and .adml files. If not, it will be created
+if (-not (Test-Path $centralStoreFolder)){
+    New-Item $centralStoreFolder -ItemType 'Directory'
+    New-Item $languageFolder -ItemType 'Directory'
+}
+
+# Copy chrome .adml and .admx files to the central store folder
+copy-item "C:\Users\$env:USERNAME\Downloads\folder\windows\admx\en-US\chrome.adml" $languageFolder
+copy-item "C:\Users\$env:USERNAME\Downloads\folder\windows\admx\chrome.admx" $centralStoreFolder
